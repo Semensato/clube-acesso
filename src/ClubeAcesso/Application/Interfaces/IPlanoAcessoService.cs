675d2a5991f4692dfc202743bfc9a6cc51da2f40
Guilherme Semensato <guilherme.semensato@mutant.com.br>
2025-07-14 16:31:35 -0300

    feat: services interfaces

---
+++ src/ClubeAcesso/Application/Interfaces/IPlanoAcessoService.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IPlanoAcessoService
+    {
+        Task<IEnumerable<PlanoAcessoDto>> ListarAsync();
+        Task<PlanoAcessoDto?> ObterPorIdAsync(Guid id);
+        Task CriarAsync(PlanoAcessoDto dto);
+        Task AtualizarAsync(Guid id, PlanoAcessoDto dto);
+        Task RemoverAsync(Guid id);
+    }
+}
