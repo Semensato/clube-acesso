7b4072e324acc431c04f26578ebb1bd352a3ca52
Guilherme Semensato <guilherme.semensato@mutant.com.br>
2025-07-14 12:39:06 -0300

    feat: domain entities

---
+++ src/ClubeAcesso/Domain/Entities/TentativaAcesso.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class TentativaAcesso
+    {
+        public Guid Id { get; set; }
+        public Guid SocioId { get; set; }
+        public Guid AreaId { get; set; }
+        public DateTime DataHora { get; set; }
+        public ResultadoTentativa Resultado { get; set; }
+    }
+}
